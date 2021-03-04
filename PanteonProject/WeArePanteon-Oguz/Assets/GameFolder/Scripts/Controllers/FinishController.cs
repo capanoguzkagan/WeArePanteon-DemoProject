dec8596d447488b84e9ffb60f48d48f9679aa544
capanoguzkagan <75337424+capanoguzkagan@users.noreply.github.com>
2021-03-04 16:54:00 +0300

    Platforms and Level Design

---
+++ PanteonProject/WeArePanteon-Oguz/Assets/GameFolder/Scripts/Controllers/FinishController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishController : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		PlayerController _player = other.GetComponent<PlayerController>();
+		if (_player!=null)
+		{
+			Debug.Log("bitti abi");
+		}
+	}
+}
---
+++ PanteonProject/WeArePanteon-Oguz/Assets/GameFolder/Scripts/Controllers/HorizontalObstacleMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalObstacleMovement : MonoBehaviour
+{
+	[SerializeField] float _objectSpeed = 4f;
+	[SerializeField] Transform[] _positions;
+	Transform _nextPos;
+	int nextPosIndex;
+	private void Awake()
+	{
+		_nextPos = _positions[0];
+	}
+	void Update()
+	{
+		MoveGameObject();
+	}
+	private void MoveGameObject()
+	{
+		if (transform.position==_nextPos.position)
+		{
+			nextPosIndex++;
+			if (nextPosIndex>=_positions.Length)
+			{
+				nextPosIndex = 0;
+			}
+			_nextPos = _positions[nextPosIndex];
+		}
+		else
+		{
+			transform.position = Vector3.MoveTowards(transform.position, _nextPos.position, _objectSpeed * Time.deltaTime);
+		}
+	}
+}
