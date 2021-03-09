4475122e463bdf3c3502192e314624733b5b9ddc
capanoguzkagan <75337424+capanoguzkagan@users.noreply.github.com>
2021-03-09 23:54:13 +0300

    Windows Build Settings / Visual Updates

---
+++ PanteonProject/WeArePanteon-Oguz/Assets/GameFolder/Scripts/Controllers/Border.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+	public float xMin, xMax, yMin, yMax, zMin, zMax;
+	void Update()
+	{
+			transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, xMin, xMax),
+			Mathf.Clamp(transform.position.y, yMin, yMax),
+			Mathf.Clamp(transform.position.z, zMin, zMax)
+			);
+	}
+}
