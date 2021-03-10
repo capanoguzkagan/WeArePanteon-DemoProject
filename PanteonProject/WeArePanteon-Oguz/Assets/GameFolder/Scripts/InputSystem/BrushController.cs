ba1cfe5aa4b6ab7a8455452b8468577c7bc51389
capanoguzkagan <75337424+capanoguzkagan@users.noreply.github.com>
2021-03-10 17:04:22 +0300

    Secret Walls and Press Mechanic Added for Paint System

--- PanteonProject/WeArePanteon-Oguz/Assets/GameFolder/Scripts/InputSystem/BrushController.cs
+++ PanteonProject/WeArePanteon-Oguz/Assets/GameFolder/Scripts/InputSystem/BrushController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PaintIn3D.Examples;
 
 public class BrushController : MonoBehaviour
 {
 	[SerializeField] private float playerSpeed = 2.0f;
 	private Player playerInput;
 	private CharacterController controller;
+	
 
 	private void Awake()
 	{
@@ -30,7 +32,17 @@ public class BrushController : MonoBehaviour
 	{
 			Vector2 movementInput = playerInput.PlayerMain.Move.ReadValue<Vector2>();
 			Vector3 move = new Vector3(movementInput.x, movementInput.y, 0f);
+			Vector3 moving = new Vector3(0f, 0f, 0.2f);
 			controller.Move(move * Time.deltaTime * playerSpeed);
+	
+			if (Input.GetMouseButtonDown(0))
+			{
+			controller.Move(moving);
+			}
+			if (Input.GetMouseButtonUp(0))
+			{
+			controller.Move((-1)*moving);
+		}
 	}
 	
 }
